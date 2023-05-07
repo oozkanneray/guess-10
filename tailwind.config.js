76e0e81787743b007c568b52f55e2211b2b64f6a
ebaytheway <lafleurr4141@gmail.com>
2023-05-07 17:15:44 +0300

    feat:add animation keyframes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        found: {
+          '0%': { color:("green") },
+        },
+        wrong: {
+          "0%" : {transform:"translate(10px)", border:("2px solid red")   },
+          "50%" : {transform:"translate(-10px)"},
+          "100%" : {transform:"translate(0px)",},
+        },
+      },
+      animation:{
+        "already-found": 'found 0.3s linear',
+        "wrong-answer": "wrong 0.2s linear",
+      }
+    },
   },
   plugins: [],
 }

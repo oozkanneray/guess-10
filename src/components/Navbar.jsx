23790f6a98f6893f7a464a9a525b706806b9831b
ebaytheway <lafleurr4141@gmail.com>
2023-05-07 17:16:53 +0300

    feat:add navbar game guide

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,31 @@
 import { useState } from "react";
 
 function Navbar() {
-  const [open, setOpen] = useState();
+
+  const openClass = "flex justify-center items-center flex-col text-gray-300 absolute h-[100vh] w-[100%] bg-black backdrop-blur-md bg-opacity-10"
+
+  const [open, setOpen] = useState(false);
 
   return (
-    <nav className="text-gray-300 bg-black h-14 flex items-center">
-      <button
-        className="text-xl bg-stone-900 rounded-xl w-8 h-8 "
-        onClick={() => {}}
-      >
-        ?
-      </button>
-    </nav>
+    <div>
+      <div className={ open ? openClass : "hidden" }>
+        <div className="w-96 h-96 bg-black rounded-2xl text-gray-300">
+          <button onClick={() => {
+            setOpen(false)
+          }}>X</button>
+        </div>
+      </div>
+      <nav className="text-gray-300 bg-black h-14 flex items-center">
+        <button
+          className="text-xl bg-stone-900 rounded-xl w-8 h-8 "
+          onClick={() => {
+            setOpen(true)
+          }}
+        >
+          ?
+        </button>
+      </nav>
+    </div>
   );
 }
 

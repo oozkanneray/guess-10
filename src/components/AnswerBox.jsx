7047f53481eb2c0d1fff6c1b7148a9152ad77045
ebaytheway <lafleurr4141@gmail.com>
2023-05-07 17:16:33 +0300

    feat:add answerbox animations

--- src/components/AnswerBox.jsx
+++ src/components/AnswerBox.jsx
@@ -1,20 +1,27 @@
 import { useEffect, useState } from "react";
 
 function AnswerBox({ answer, userInput }) {
+  let initialClass =
+    "text-3xl text-gray-300 border-2 border-gray-300 w-44 h-12 text-center m-2 rounded-xl";
+  let animClass =
+    "text-3xl text-gray-300 border-2 border-gray-300 w-44 h-12 text-center m-2 rounded-xl animate-already-found";
+
   const [find, setFind] = useState(false);
+  const [founded, setFounded] = useState(initialClass);
 
   useEffect(() => {
     checkAnser();
   }, [userInput]);
 
   const checkAnser = () => {
-    if (userInput.toLowerCase() == answer.toLowerCase()) setFind(true);
+    if (userInput.toLowerCase() === answer.toLowerCase()) {
+      setFind(true);
+      setFounded(animClass);
+    }
   };
 
   return (
-    <div className="text-2xl text-gray-300">
-      {find ? <div>{answer}</div> : <div>?</div>}
-    </div>
+    <div className={founded}>{find ? <div>{answer}</div> : <div>?</div>}</div>
   );
 }
 

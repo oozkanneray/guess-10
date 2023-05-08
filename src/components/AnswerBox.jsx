0576faf658dfc9bb325c0e36f482b8b3cbb3d30c
ebaytheway <lafleurr4141@gmail.com>
2023-05-08 21:39:17 +0300

    feat:add update score

--- src/components/AnswerBox.jsx
+++ src/components/AnswerBox.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-function AnswerBox({ answer, userInput }) {
+function AnswerBox({ answer, userInput,setScore,timer,score}) {
   let initialClass =
     "text-3xl text-gray-300 border-2 border-gray-300 w-44 h-12 text-center m-2 rounded-xl";
   let animClass =
@@ -13,10 +13,16 @@ function AnswerBox({ answer, userInput }) {
     checkAnser();
   }, [userInput]);
 
+  const updateScore = () => {
+    let changeScore = score + (timer * 10)
+    setScore(changeScore)
+  }
+
   const checkAnser = () => {
     if (userInput.toLowerCase() === answer.toLowerCase()) {
       setFind(true);
       setFounded(animClass);
+      updateScore()
     }
   };
 

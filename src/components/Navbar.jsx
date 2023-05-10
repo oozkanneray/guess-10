3c8a955ef8a5ce6893207315d7ef998c90459d4f
ebaytheway <lafleurr4141@gmail.com>
2023-05-10 20:35:25 +0300

    feat: add game header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,15 +10,16 @@ function Navbar() {
   return (
     <div>
       <GuideCard open={open} setOpen={setOpen}/>
-      <nav className="text-gray-300 bg-black h-14 flex items-center">
+      <nav className="text-gray-300 bg-black h-14 flex items-center justify-center">
         <button
-          className="text-xl bg-stone-900 rounded-xl w-8 h-8 "
+          className="text-xl bg-stone-900 rounded-xl w-8 h-8 mx-3 "
           onClick={() => {
             setOpen(true);
           }}
         >
           ?
         </button>
+        <h4 className="text-3xl text-questionTextColor mx-5">Guess10</h4>
         <div className="text-xl bg-stone-900 rounded-xl w-8 h-8 flex items-center justify-center text-center mx-3">
           <a href="https://github.com/oozkanneray/guess-10"><img className="text-xlrounded-xl w-6 h-6" src={githubPng}></img></a>
         </div>

134488f45b092fd027f746b2a88652ef9f0b6655
ebaytheway <lafleurr4141@gmail.com>
2023-05-10 20:11:18 +0300

    change:remove guide,add github icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,15 @@
 import { useState } from "react";
+import githubPng from "../assets/githubPng.png";
+import GuideCard from "./GuideCard";
 
 function Navbar() {
-  const openClass ="absolute flex justify-center items-center flex-col text-gray-300 absolute h-[100vh] w-[100%] bg-black backdrop-blur-md bg-opacity-10 animate-opening";
-  const closeAnimClas = "absolute flex justify-center items-center flex-col text-gray-300 absolute h-[100vh] w-[100%] bg-black backdrop-blur-md bg-opacity-10 animate-closing"
-  
-  const [open, setOpen] = useState(false);
+
+
+  const [open, setOpen] = useState(true);
 
   return (
     <div>
-      <div className={open ? openClass : closeAnimClas}>
-        <div className="w-96 bg-currentBgColor rounded-2xl text-gray-300 shadow-lg shadow-black p-5">
-          <div className="flex justify-around m-4">
-            <h4 className="text-4xl">Guess10</h4>
-          <button
-          className="text-xl"
-            onClick={() => {
-              setOpen(false);
-            }}
-          >
-            X
-          </button>
-          </div>
-          <div className="flex flex-col text-lg py-5">
-            <p className="mb-4">Size verilen soruyu 60 saniye içerisinde ne kadar cevaplarsanız o kadar puan alırsınız.</p>
-            <p className="mb-4">Kalan sürenizin kaç olduğuna bağlı olarak puan kazanırsınız. (Kalan süre * 10) </p>
-            <p className="mb-4">Eğer süreniz biterse puan alamazsınız fakat cevapları tahmin etmeye devam edebilirsiniz. </p>
-            <p className="mb-4">Doyasıya eğlenmeye bakın nasıl olsa ana amaç puan almak değil...</p>
-          </div>
-        </div>
-      </div>
+      <GuideCard open={open} setOpen={setOpen}/>
       <nav className="text-gray-300 bg-black h-14 flex items-center">
         <button
           className="text-xl bg-stone-900 rounded-xl w-8 h-8 "
@@ -38,6 +19,9 @@ function Navbar() {
         >
           ?
         </button>
+        <div className="text-xl bg-stone-900 rounded-xl w-8 h-8 flex items-center justify-center text-center mx-3">
+          <a href="https://github.com/oozkanneray/guess-10"><img className="text-xlrounded-xl w-6 h-6" src={githubPng}></img></a>
+        </div>
       </nav>
     </div>
   );

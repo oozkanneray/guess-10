b02418abfaf4f8b64c2b081af242fa3fd5b30910
ebaytheway <lafleurr4141@gmail.com>
2023-05-10 21:45:33 +0300

    feat:add info data

--- src/data.jsx
+++ src/data.jsx
@@ -1,7 +1,19 @@
 const data = [
     {
         question: "Dünyanın en çok nüfusa sahip ilk 10 ülkesi?",
-        answers:["Çin","Hindistan","Amerika","Endonezya","Pakistan","Brezilya","Nijerya","Bangladeş","Rusya","Meksika"]
+        answers:[
+        {answer:"Çin",info:"1.411.778.724"},
+        {answer:"Hindistan",info:"1.394.967.309"},
+        {answer:"Amerika",info:"332.864.341"},
+        {answer:"Endonezya",info:"271.350.000"},
+        {answer:"Pakistan",info:"225.200.000"},
+        {answer:"Brezilya",info:"216.332.229"},
+        {answer:"Nijerya",info:"211.401.000"},
+        {answer:"Bangladeş",info:"171.854.414"},
+        {answer:"Rusya",info:"146.171.015"},
+        {answer:"Meksika",info:"126.014.024"},
+        
+    ]
     }
 ]
 

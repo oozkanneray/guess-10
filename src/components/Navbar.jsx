57fa0bfb2502f556dcafea3f6c5485782cdf0c4c
ebaytheway <lafleurr4141@gmail.com>
2023-05-20 20:03:49 +0300

    docs:code change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import githubPng from "../assets/githubPng.png";
 import GuideCard from "./GuideCard";
+import NewQuestion from "./NewQuesition";
 
 function Navbar() {
-  
   const [open, setOpen] = useState(true);
+  const [addnew, setAddnew] = useState(false);
 
   return (
     <div>
-      <GuideCard open={open} setOpen={setOpen}/>
+      {open && <GuideCard open={open} setOpen={setOpen} />}
+      {addnew && <NewQuestion addnew={addnew} setAddnew={setAddnew}/>}
       <nav className="text-gray-300 bg-black h-14 flex items-center justify-center">
         <button
           className="text-xl bg-stone-900 rounded-xl w-8 h-8 mx-3 "
@@ -18,10 +20,14 @@ function Navbar() {
         >
           ?
         </button>
-        <button className="flex items-center justify-center font-bold text-2xl bg-stone-900 rounded-xl w-8 h-8 mx-3">+</button>
+        <button onClick={() =>{setAddnew(true)}} className="flex items-center justify-center font-bold text-2xl bg-stone-900 rounded-xl w-8 h-8 mx-3">
+          +
+        </button>
         <h4 className="text-3xl text-questionTextColor mx-5">Guess10</h4>
         <div className="text-xl bg-stone-900 rounded-xl w-8 h-8 flex items-center justify-center text-center mx-3">
-          <a href="https://github.com/oozkanneray/guess-10"><img className="text-xlrounded-xl w-6 h-6" src={githubPng}></img></a>
+          <a href="https://github.com/oozkanneray/guess-10">
+            <img className="text-xlrounded-xl w-6 h-6" src={githubPng}></img>
+          </a>
         </div>
       </nav>
     </div>
